b827c4a60fca302cea1459a4fc0fc8aa4c87e119
Manish <mbjani2@gmail.com>
2025-02-24 16:27:12 +1100

    new changes till lesson 57

--- variables.tf
+++ variables.tf
@@ -11,25 +11,25 @@ variable "ftp_port" {
 }
 
 variable "available-ami" {
-  type = list
-  default = ["ami-09b42976632b27e9b","ami-01ab4a3bc7e75f44a","ami-0d783a243942fbe54"]
+  type    = list(any)
+  default = ["ami-09b42976632b27e9b", "ami-01ab4a3bc7e75f44a", "ami-0d783a243942fbe54"]
 }
 
 variable "instance_type" {
-  type = list
+  type    = list(any)
   default = ["t2.micro", "t2.medium", "m5.large"]
 }
 
 
 variable "default" {
-  type = map
-  default =  {
-    Project = "DevOps"
+  type = map(any)
+  default = {
+    Project     = "DevOps"
     Environment = "Test"
   }
 }
 
 variable "tags" {
-  type = list
+  type    = list(any)
   default = ["dev", "test", "prod"]
 }

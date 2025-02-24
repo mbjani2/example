e822feac8d7a0c6438ff262926e0358b17a8ee2b
Manish <mbjani2@gmail.com>
2025-02-24 16:19:56 +1100

    added new changes and functions - till lesson 57

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,14 +1,10 @@
-provider "aws" {
-  region = "ap-southeast-2"
-}
-
-provider "azurerm" {}
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-0d6560f3176dc9ec0"
-  instance_type = var.instance_type
-  vpc_security_group_ids = "sg-050f1618a9c002599", "sg-08c6feaeb8f5ce6e3"
-  tags = {
-    Name = "my-first-instance"
-  }
+  ami           = element(var.available-ami,count.index)
+  instance_type = element(var.instance_type, count.index)
+  vpc_security_group_ids = ["sg-050f1618a9c002599", "sg-08c6feaeb8f5ce6e3"]
+  count = length(var.tags)
+  tags = merge(var.default, {
+    Name = "nginx-server-${element(var.tags,count.index)}"
+})
 }

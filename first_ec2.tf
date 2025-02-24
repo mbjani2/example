8567aa49d712c9fbd5b9bdd375ee0ab6562c54bb
Manish <mbjani2@gmail.com>
2025-02-24 16:27:12 +1100

    new changes till lesson 57

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,10 +1,10 @@
 
 resource "aws_instance" "ec2" {
-  ami           = element(var.available-ami,count.index)
-  instance_type = element(var.instance_type, count.index)
+  ami                    = element(var.available-ami, count.index)
+  instance_type          = element(var.instance_type, count.index)
   vpc_security_group_ids = ["sg-050f1618a9c002599", "sg-08c6feaeb8f5ce6e3"]
-  count = length(var.tags)
+  count                  = length(var.tags)
   tags = merge(var.default, {
-    Name = "nginx-server-${element(var.tags,count.index)}"
-})
+    Name = "nginx-server-${element(var.tags, count.index)}"
+  })
 }

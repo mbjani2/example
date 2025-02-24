7c4b235bfc805bfb7596dfa55ba3ac5eed7f8bce
Manish <mbjani2@gmail.com>
2025-02-24 11:24:22 +1100

    uncommitted chnges

--- first_ec2.tf
+++ first_ec2.tf
@@ -7,8 +7,8 @@ provider "azurerm" {}
 resource "aws_instance" "ec2" {
   ami           = "ami-0d6560f3176dc9ec0"
   instance_type = var.instance_type
+  vpc_security_group_ids = "sg-050f1618a9c002599", "sg-08c6feaeb8f5ce6e3"
   tags = {
     Name = "my-first-instance"
   }
 }
-##

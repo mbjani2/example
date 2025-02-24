f27496248edf5386300403c855eb81e99032dfdc
Manish <mbjani2@gmail.com>
2025-02-24 16:19:56 +1100

    added new changes and functions - till lesson 57

--- variables.tf
+++ variables.tf
@@ -10,6 +10,26 @@ variable "ssh_port" {
 variable "ftp_port" {
 }
 
+variable "available-ami" {
+  type = list
+  default = ["ami-09b42976632b27e9b","ami-01ab4a3bc7e75f44a","ami-0d783a243942fbe54"]
+}
+
 variable "instance_type" {
-  default = "t2.micro"
+  type = list
+  default = ["t2.micro", "t2.medium", "m5.large"]
+}
+
+
+variable "default" {
+  type = map
+  default =  {
+    Project = "DevOps"
+    Environment = "Test"
+  }
+}
+
+variable "tags" {
+  type = list
+  default = ["dev", "test", "prod"]
 }

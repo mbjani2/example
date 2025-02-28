deb28e66af715b3958ac977bbbf7fd11a37c2b5b
Manish <mbjani2@gmail.com>
2025-02-28 14:12:26 +1100

    added changes related to dynamic blocks

--- firewall.tf
+++ firewall.tf
@@ -12,3 +12,18 @@ resource "aws_security_group" "web-server" {
     }
   }
 }
+
+resource "aws_security_group" "management" {
+  name = "Management-group"
+
+  dynamic "ingress" {
+    for_each = var.mg_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+}

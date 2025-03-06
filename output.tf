97cebfcdd0606aa1f2c124387ee9a8c1f4698176
Manish <mbjani2@gmail.com>
2025-03-07 09:53:46 +1100

    made changes for K8s

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "public_ip" {
-  value = aws_eip.instance1
+  value = aws_eip.instance1.public_ip
+}
+
+# output "instance_name" {
+#   value = aws_instance.ec2[*].tags.Name
+# }
+
+# output "instance_ip" {
+#   value = aws_instance.ec2[*].arn
+# }
+
+output "combined" {
+  value = zipmap(aws_instance.ec2[*].tags.Name, aws_instance.ec2[*].arn)
 }

8e687a2108e67ac40389727b410bafe4427add98
Manish <mbjani2@gmail.com>
2025-03-07 11:29:31 +1100

    changed posrt for k8

--- firewall.tf
+++ firewall.tf
@@ -27,3 +27,9 @@ resource "aws_security_group" "management" {
   }
 
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.web-server.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}

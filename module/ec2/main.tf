5d1a535aa4f6e5278fa498d8af718575e552c636
Manish <mbjani2@gmail.com>
2025-03-19 16:16:04 +1100

    adding changin till module - lesson 115

---
+++ module/ec2/main.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "myec2" {
+    ami = var.ami
+    instance_type = var.instance_type
+
+}
---
+++ teams/A/local.tf
@@ -0,0 +1,7 @@
+locals {
+    instance_type = {
+        default = "t2.nano",
+        dev = "t2.micro",
+        prod = "t3.micro"
+    }
+}
